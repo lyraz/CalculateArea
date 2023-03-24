c033a5dc93d7e745b41b4a403d2640c1959bc491
lyraz <admin@leotech.co.nz>
2023-03-25 00:29:16 +1300

    Initial commit

---
+++ target/debug/CalculateArea.d
@@ -0,0 +1,1 @@
+/Users/topnewgod/CalculateArea/target/debug/CalculateArea: /Users/topnewgod/CalculateArea/src/main.rs
---
+++ target/debug/deps/CalculateArea-5a73bb443ca138dd.d
@@ -0,0 +1,5 @@
+/Users/topnewgod/CalculateArea/target/debug/deps/CalculateArea-5a73bb443ca138dd: src/main.rs
+
+/Users/topnewgod/CalculateArea/target/debug/deps/CalculateArea-5a73bb443ca138dd.d: src/main.rs
+
+src/main.rs:
